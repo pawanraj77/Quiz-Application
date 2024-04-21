788a7df7cdf3953437c42cd1c4eccffd2ff2651f
Pawan Raj <pawansingh201326@gmail.com>
2024-04-21 22:33:59 +0530

    initial commit

---
+++ src/test/java/com/pawan/quizApp/QuizAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawan.quizApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
